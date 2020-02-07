887cbe5e57746c83eb551438047ca431f5cc9fea
Tien Pham <tien@ph6m.com>
2020-02-07 09:38:05 +0700

    Implement keyframes helper

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,59 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Library from 'react-native-easing-keyframes'
+import React, { useEffect } from 'react'
+import { StyleSheet, View, Animated, Easing, StatusBar } from 'react-native'
+import keyframes from 'react-native-easing-keyframes'
 
 export default function App() {
+  const [value] = React.useState(new Animated.Value(0))
+  const [animation] = React.useState(
+    Animated.loop(
+      Animated.timing(value, {
+        duration: 1200,
+        toValue: 100,
+        easing: keyframes({
+          keyframes: [0, 50, 100],
+          easing: Easing.bezier(0.42, 0.0, 0.58, 1.0),
+        }),
+        useNativeDriver: true,
+      })
+    )
+  )
+
+  useEffect(() => {
+    animation.start()
+
+    return () => {
+      value.setValue(0)
+      animation.stop()
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>{Library}</Text>
+      <StatusBar barStyle="light-content" />
+      <Animated.View
+        style={[
+          styles.plane,
+          {
+            transform: [
+              {
+                perspective: 48 * 3,
+              },
+              {
+                rotateX: value.interpolate({
+                  inputRange: [0, 50, 100],
+                  outputRange: ['0.1deg', '-179.9deg', '-179.9deg'],
+                }),
+              },
+              {
+                rotateY: value.interpolate({
+                  inputRange: [0, 50, 100],
+                  outputRange: ['0.1deg', '0.1deg', '-179.9deg'],
+                }),
+              },
+            ],
+          },
+        ]}
+      />
     </View>
   )
 }
@@ -13,8 +61,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d35400',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  plane: {
+    width: 48,
+    height: 48,
+    backgroundColor: '#fff',
   },
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,57 @@
-export default 'Hello'
+import { EasingFunction, Easing } from 'react-native'
+
+export interface Config {
+  keyframes: number[]
+  easing?: EasingFunction
+  easingsByKeyframe?: { [key: number]: EasingFunction }
+}
+
+export default function keyframes({
+  keyframes,
+  easing = Easing.inOut(Easing.ease),
+  easingsByKeyframe = {},
+}: Config) {
+  validateKeyframes(keyframes)
+
+  return (t: number) => {
+    const lastKeyframe = keyframes[keyframes.length - 1]
+    for (
+      let keyframeIndex = 1;
+      keyframeIndex < keyframes.length;
+      keyframeIndex++
+    ) {
+      if (t < keyframes[keyframeIndex] / lastKeyframe) {
+        const prev = keyframes[keyframeIndex - 1] / lastKeyframe
+        const current =
+          (keyframes[keyframeIndex] - keyframes[keyframeIndex - 1]) /
+          lastKeyframe
+        const currentEasing =
+          easingsByKeyframe[keyframes[keyframeIndex - 1]] || easing
+
+        return prev + currentEasing((t - prev) / current) * current
+      }
+    }
+    return t
+  }
+}
+
+function validateKeyframes(keyframes: number[]) {
+  if (keyframes.length < 3) {
+    throw Error('keyframes must have at least 3 items')
+  }
+
+  for (let i = 0; i < keyframes.length; i++) {
+    const currentKeyframe = keyframes[i]
+
+    if (isNaN(currentKeyframe)) {
+      throw Error('A keyframe value must be a number')
+    }
+
+    if (i > 0) {
+      const prevKeyframe = keyframes[i - 1]
+      if (currentKeyframe <= prevKeyframe) {
+        throw Error('keyframes must be monotonically increasing ' + keyframes)
+      }
+    }
+  }
+}
